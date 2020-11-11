5c2d29b50de8c268993a3b5539969997daad41a7
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-10 23:23:47 -0500

    added switch statements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './jonathanrivera.jpg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My name is Jonathan Rivera and I'm going to master React!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/JonathanR5000"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
-    </div>
+    <main>
+      <Switch>
+        
+      </Switch>
+    </main>
+
   );
 }
-
+{    // <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       My name is Jonathan Rivera and I'm going to master React!!!
+//     </p>
+//     <a
+//       className="App-link"
+//       href="https://github.com/JonathanR5000"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Check Me Out!
+//     </a>
+//   </header>
+// </div>}
 export default App;
