43c16e14a0e01108741a9ae872c2d8699b916e2a
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-14 16:38:17 -0500

    added resume button and description content of about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,13 @@ import '../../App.css';
 
 function About(){
     return(
-        <h1 class="about-heading">Testing</h1>
+        <div class="about-container">
+            <h1 class="about-heading">About Me</h1>
+            <p class="description">A senior at Lehman College, looking into stepping into
+            the world of software development. I love to be a leader who anyone can look up to, 
+            and believe in themselves, that they too can suceed in life.</p>
+            <div class="resume-button">Resume</div>
+        </div>
     );
 }
 
