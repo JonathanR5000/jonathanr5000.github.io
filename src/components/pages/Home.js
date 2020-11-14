74180b5faeb9e577a0e102ed53bc5e0770bf3a64
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-14 14:52:28 -0500

    fixed layout of home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,9 +5,13 @@ import Logo from '../../jonathanrivera.jpg';
 function Home(){
     return(
         <div class="container">
-            <h1 class="name-style">Jonathan Rivera</h1>
-            <p>Software Developer</p>
-            <img src={Logo} class="App-logo"></img>
+            <div class="left-row">
+                <h1 class="name-style">Jonathan Rivera</h1>
+                <p class="name-title">Inspiring Software Developer</p>
+            </div>
+            <div class="right-row">
+                <img src={Logo} class="App-logo"></img>
+            </div>
         </div>
     );
 }
