10e39018291b7248840b0f17e5855c6904b11161
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-14 21:11:47 -0500

    adding home links to github and linkedIN soon

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import Logo from '../../jonathanrivera.jpg';
+import LinkedIN from '../../linkedIN.jpg';
 
 function Home(){
     return(
@@ -8,6 +9,10 @@ function Home(){
             <div class="left-row">
                 <h1 class="name-style">Jonathan Rivera</h1>
                 <p class="name-title">Inspiring Software Developer</p>
+                <div class="about-links">
+                    {/* <img src={LinkedIN} href="#"></img> */}
+                    {/* Include anchor or icon links to github, and linkedIN*/}
+                </div>
             </div>
             <div class="right-row">
                 <img src={Logo} class="App-logo"></img>
