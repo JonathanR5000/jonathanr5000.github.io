ddf01fc98ab1482823199c4e298cf71b3976b19d
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-20 23:07:00 -0500

    added google font ubuntu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
                 <Link class="links" to="/" style={{textDecoration: 'none'}}> Home </Link>
                 <Link class="links" to="/about" style={{textDecoration: 'none'}}> About Me </Link>
                 <Link class="links" to="/projects" style={{textDecoration: 'none'}}> Projects </Link>
-                <Link class="links" to="/education" style={{textDecoration: 'none'}}> Education </Link>
+                {/* <Link class="links" to="/education" style={{textDecoration: 'none'}}> Education </Link> */}
                 <Link class="links" to="/contact" style={{textDecoration: 'none'}}> Contact </Link>
             </div>
         </div>
