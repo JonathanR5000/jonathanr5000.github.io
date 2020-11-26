53eeccf1d8226ede3cf017b898cd2b5431c6c4b9
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-26 13:35:08 -0500

    added handleChange and handleSubmit

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -17,6 +17,25 @@ class Contact extends React.Component{
         }
     }
 
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({
+            disabled: true,
+            emailSent: true 
+        })
+    }
+
     render() {
         return(
         <div class="contact-container">
