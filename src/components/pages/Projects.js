78739c795a0f11b9d98ab6faf0e7df45ddbd3af1
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-26 18:20:54 -0500

    added project images and descriptions, as well as links to github and live pages

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -18,15 +18,18 @@ class Projects extends Component{
                 <div class="project-container">
                 {/* Project 1 */}
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}> Natours </CardTitle>
+                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}>
+                    <img class="card-img-top" src={Natours} alt="Card image cap"></img>
+                    </CardTitle>
                     <CardText>
+                    <h5> Natours </h5>
                         Natours is a fictional resort tour, that provides tours for customers 
                         and includes various packages that the user can choose. The website also has an option 
                         to choose the size of the group that will be participating in the tour. 
                     </CardText>
                     <CardActions border>
-                        <Button colored> Github </Button>
-                        <Button colored> Live </Button>
+                        <Button colored href="https://github.com/JonathanR5000/Natours" target="_blank"> Github </Button>
+                        <Button colored href="https://jonathanr5000.github.io/Natours/" target="_blank"> Live </Button>
                     </CardActions>
                     <CardMenu style={{color: "black"}}>
                         <IconButton name=""></IconButton>
@@ -35,36 +38,40 @@ class Projects extends Component{
                 </Card>
 
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}> Omnifood </CardTitle>
+                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}> 
+                    <img class="card-img-top" src={Omnifood} alt="Card image cap"></img>
+                    </CardTitle>
                     <CardText>
+                    <h5> Omnifood </h5>
                     Omnifood is a fictional food delivery company, 
                     that provides food delivery services that serve whatever the customer feels 
                     like eating such as pizza, ramen, steak, burgers, soup, dessert, and many more delishes orders. 
                     </CardText>
                     <CardActions border>
-                        <Button colored> Github </Button>
-                        <Button colored> Live </Button>
+                        <Button colored href="https://github.com/JonathanR5000/Omnifood" target="_blank"> Github </Button>
+                        <Button colored href="https://jonathanr5000.github.io/Omnifood/" target="_blank"> Live </Button>
                     </CardActions>
                     <CardMenu style={{color: "black"}}>
-                        <IconButton name=""></IconButton>
                     </CardMenu>
                     
                 </Card>
 
                 <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}> Nexter </CardTitle>
+                    <CardTitle style={{color:'black', height: '176px', background: 'url() center /cover'}}>
+                    <img class="card-img-top" src={Nexter} alt="Card image cap"></img>
+                    </CardTitle>
                     <CardText>
+                    <h5> Nexter </h5>
                     Nextor is a fictional real estate company, 
                     who's mission is to help you find your dream home! 
                     The purposes of the website is to practice advanceds CSS methods such as CS grid, and SASS 
                     to help better showcase each image effectively.
                     </CardText>
                     <CardActions border>
-                        <Button colored> Github </Button>
-                        <Button colored> Live </Button>
+                        <Button colored href="https://github.com/JonathanR5000/Nexter" target="_blank"> Github </Button>
+                        <Button colored href="https://jonathanr5000.github.io/Nexter/" target="_blank"> Live </Button>
                     </CardActions>
                     <CardMenu style={{color: "black"}}>
-                        <IconButton name=""></IconButton>
                     </CardMenu>
                     
                 </Card>
@@ -110,52 +117,4 @@ class Projects extends Component{
         );
     }
 }
-    {/* // <div class="project-container">
-
-        
-
-//     <div class="card-group">
-//         <div class="card">
-//             <img class="card-img-top" src={Natours} alt="Card image cap"></img>
-//             <div class="card-body">
-//             <h5 class="card-title">Natours</h5>
-//             <p class="card-text">Natours is a fictional resort tour, that provides tours for customers 
-//             and includes various packages that the user can choose. The website also has an option 
-//             to choose the size of the group that will be participating in the tour. 
-//             </p>
-//             <a href="https://github.com/JonathanR5000/Natours" class="card-link" target="_blank">Github</a>
-//             <a href="https://jonathanr5000.github.io/Natours/" class="card-link" target="_blank">Live</a>
-//             </div>
-//         </div>
-
-//         <div class="card">
-//             <img class="card-img-top" src={Omnifood} alt="Card image cap"></img>
-//             <div class="card-body">
-//             <h5 class="card-title">Omnifood</h5>
-//             <p class="card-text">Omnifood is a fictional food delivery company, 
-//             that provides food delivery services that serve whatever the customer feels 
-//             like eating such as pizza, ramen, steak, burgers, soup, dessert, and many more delishes orders. 
-//             </p>
-//             <a href="https://github.com/JonathanR5000/Omnifood" class="card-link" target="_blank">Github</a>
-//             <a href="https://jonathanr5000.github.io/Omnifood/" class="card-link" target="_blank">Live</a>
-//             </div>
-//         </div>
-
-//             <div class="card">
-//             <img class="card-img-top" src={Nexter} alt="Card image cap"></img>
-//             <div class="card-body">
-//             <h5 class="card-title">Nexter</h5>
-//             <p class="card-text">Nextor is a fictional real estate company, 
-//             who's mission is to help you find your dream home! 
-//             The purposes of the website is to practice advanceds CSS methods such as CS grid, and SASS 
-//             to help better showcase each image effectively.</p>
-//             <a href="https://github.com/JonathanR5000/Nexter" class="card-link" target="_blank">Github</a>
-//             <a href="https://jonathanr5000.github.io/Nexter/" class="card-link" target="_blank">Live</a>
-//             </div>
-//         </div>
-//     </div>
-    
-
-// </div> */}
-
 export default Projects;
