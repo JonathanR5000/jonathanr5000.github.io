6fb2daecd1b06aeda41433b021e7c35a9666fd22
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-26 12:59:12 -0500

    added form and button divs for contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import Axios from 'axios';
 import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 class Contact extends React.Component{
 
@@ -23,10 +24,28 @@ class Contact extends React.Component{
                 Let's Chat
             </h1>
             <div class="contact-content">
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Group>
-                        <Form.Label>Full Name</Form.Label>
+                        <Form.Label htmlFor="full-Name">Full Name</Form.Label>
+                        <Form.Control id="full-Name" name="name" type="text" value={this.state.name} onChange={this.handleChange}></Form.Control>
                     </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label htmlFor="message">Message</Form.Label>
+                        <Form.Control id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleChange}></Form.Control>
+                    </Form.Group>
+
+                    <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+                        Send
+                    </Button>
+
+                    {this.state.emailSent === true && <p className="d-inline successs-msg">Email Sent!</p>}
+                    {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent!</p>}
                 </Form>
             </div>
         </div>
