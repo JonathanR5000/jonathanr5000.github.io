d3e51de4086743484125eae7604a8848629bd4f6
JonathanR5000 <jonathan.rivera4@lc.cuny.edu>
2020-11-26 19:14:08 -0500

    fixed typing animation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,7 +7,9 @@ function Home(){
         <div class="home-container">
             <div class="left-row">
                 <h1 class="name-style">Jonathan Rivera</h1>
-                <p class="name-title anim-typewriter">Inspiring Software Developer</p>
+                <div class="text-animation">
+                <p class="name-title" data-text="text-anim">Inspiring Software Developer</p>
+                </div>
                 <div class="about-links">
                     <a class="about-btn linkedIN" href="https://www.linkedin.com/in/jonathanrivera5000/" target="_blank"> LinkedIN </a>
                     <a class="about-btn github" href="https://github.com/JonathanR5000" target="_blank"> Github </a>
